9ed25d296d5d353ea6721ea6dd7a1300de9dc356
shuo.wang@ksudi.com <Wangshuo>
2017-09-06 11:05:18 +0800

    将项目拆成分布式服务, 并实现单点登录

---
+++ springcloud-common/src/main/java/com/example/SpringcloudConfigApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+@ComponentScan(basePackages = "com.example")
+public class SpringcloudConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigApplication.class, args);
+    }
+}
